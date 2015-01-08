2d13966b698136e8d7e32c8254f149782f5a70ac
fishermanswharff <fishermanswharff@github.com>
2015-01-08 01:40:04 -0500

    Autopush GradingWizard Api Master Branch

--- db/migrate/20150108060535_create_pull_requests.rb
+++ db/migrate/20150108060535_create_pull_requests.rb
@@ -1,7 +1,15 @@
 class CreatePullRequests < ActiveRecord::Migration
   def change
     create_table :pull_requests do |t|
-      
+      t.integer :unique_id, unique: true, null: false
+      t.string :name
+      t.integer :build_status
+      t.string :status_message
+      t.string :build_url
+      t.string :commit_message
+      t.integer :pull_request_number
+      t.string :committer_name
+      t.timestamps
     end
   end
 end
