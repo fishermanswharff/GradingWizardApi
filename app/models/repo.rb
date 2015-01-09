bc1ecef4307828fb96a6012ca6b44b214ca8412b
fishermanswharff <fishermanswharff@github.com>
2015-01-09 11:05:46 -0500

    working out redis

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -3,28 +3,28 @@ class Repo < ActiveRecord::Base
   has_many :pull_requests
 
   def self.github_repos
-    repos = {}
-    repos[:pages] = []
-    client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
-    first_page = client.repos('ga-wdi-boston', :per_page => 100)
-    repos[:pages] << first_page
-    last_response = client.last_response
-    number_of_pages = last_response.rels[:last].href.match(/page=(\d+)$/)[1].to_i
-    loop do
-      last_response = last_response.rels[:next].get
-      repos[:pages] << last_response.data
-      break if last_response.rels[:next].nil?
-    end
-    repos[:pages].map { |page|  
-      page.map { |repo|
-        if Repo.all.length > 0
-          Repo.find_by(name: repo[:name]).nil? ? Repo.create!({name: repo[:name], url: repo[:html_url], github_data: repo.to_json}) : break
-        else
-          Repo.create!({name: repo[:name], url: repo[:html_url], github_data: repo.to_json})
-        end
+    Rails.cache.fetch([:ga_repos,self], expires_in: 5.minutes) do
+      repos = {}
+      repos[:pages] = []
+      client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+      first_page = client.repos('ga-wdi-boston', :per_page => 100)
+      repos[:pages] << first_page
+      last_response = client.last_response
+      number_of_pages = last_response.rels[:last].href.match(/page=(\d+)$/)[1].to_i
+      loop do
+        last_response = last_response.rels[:next].get
+        repos[:pages] << last_response.data
+        break if last_response.rels[:next].nil?
+      end
+      repos[:pages].map { |page|  
+        page.map { |repo|
+          if Repo.all.length > 0
+            Repo.find_by(name: repo[:name]).nil? ? Repo.create!({name: repo[:name], url: repo[:html_url], github_data: repo.to_json}) : break
+          else
+            Repo.create!({name: repo[:name], url: repo[:html_url], github_data: repo.to_json})
+          end
+        }
       }
-    }
-    # Rails.cache.fetch([:ga_repos,self], expires_in: 5.minutes) do
-    # end
+    end
   end
 end
