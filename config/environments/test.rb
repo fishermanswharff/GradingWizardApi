d1856c24e617fccda7d95b863a32cdaea7e0af64
fishermanswharff <fishermanswharff@github.com>
2015-01-09 00:37:01 -0500

    Autopush GradingWizard Api Master Branch

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+  # config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
